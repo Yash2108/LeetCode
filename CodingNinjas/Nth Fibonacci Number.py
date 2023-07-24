9e7d701ce3859518ccc00b72d698681957a4a5f1
Yash Jain <1999yashjain@gmail.com>
2023-07-24 16:28:41 -0400

    Time: 183 ms; Memory: 6994 kB

--- CodingNinjas/Nth Fibonacci Number.py
+++ CodingNinjas/Nth Fibonacci Number.py
@@ -6,14 +6,17 @@ from os import *
 ## Read input as specified in the question.
 ## Print output as specified in the question.
 
-n=int(input())
-if n<=2:
-    print(1)
-else:
-    val1=1
-    val2=1
-    for i in range(n-2):
-        val3=val1+val2
-        val1=val2
-        val2=val3
-    print(val3)
+def fibonacci(n):
+    n=int(n)
+    if n in map_fib:
+        return map_fib[n]
+    
+    val=fibonacci(n-1)+fibonacci(n-2)
+    map_fib[n]=val
+    return val
+
+map_fib={
+    1:1,
+    2:1
+}
+print(fibonacci(input()))
