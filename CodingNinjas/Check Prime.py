07fae860d5b5c43a2d9bb4690c32e1222bfb86a1
Yash Jain <1999yashjain@gmail.com>
2023-07-25 22:17:13 -0400

    Optimised

--- CodingNinjas/Check Prime.py
+++ CodingNinjas/Check Prime.py
@@ -1,10 +1,12 @@
+from math import sqrt
+
 n=int(input())
 
 if n==1:
     print("NO")
 else:
     flag=True
-    for i in range(2, n):
+    for i in range(2, int(sqrt(n))+1):
         if n%i==0:
             flag=False
 
