0c860be272374348b0ab65483fe198fbdef15b0d
Yash Jain <36790403+Yash2108@users.noreply.github.com>
2024-03-22 14:44:56 -0700

    Time: 58 ms (10.21%) | Memory: 17.2 MB (40.57%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,13 +1,14 @@
 from string import punctuation
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        if len(s)==0:
+            return True
         s=''.join([i for i in s if i not in punctuation and i!=' ']).lower()
-        i=0
-        j=len(s)-1
-        while i<=j:
-            print(i, j)
-            if s[i]!=s[j]:
+        # i=0
+        # j=len(s)-1
+        for i in range(len(s)//2):
+            if s[i]!=s[-i-1]:
                 return False
-            i+=1
-            j-=1
+            # i+=1
+            # j-=1
         return True
