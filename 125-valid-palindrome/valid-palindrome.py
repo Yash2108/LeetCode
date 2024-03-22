bc386d47b0e73e73af4687a7ee1c2efd181adb4b
Yash Jain <36790403+Yash2108@users.noreply.github.com>
2024-03-22 14:47:28 -0700

    Time: 46 ms (51.33%) | Memory: 17.3 MB (40.57%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -3,11 +3,7 @@ class Solution:
         if len(s)==0:
             return True
         s=''.join([i for i in s if i.isalnum()]).lower()
-        # i=0
-        # j=len(s)-1
         for i in range(len(s)//2):
             if s[i]!=s[-i-1]:
                 return False
-            # i+=1
-            # j-=1
         return True
