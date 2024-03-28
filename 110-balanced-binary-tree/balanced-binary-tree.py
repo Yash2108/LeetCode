a1fdc6a7ca7f805a6b5735c93f907d930e7f0eb0
Yash Jain <36790403+Yash2108@users.noreply.github.com>
2024-03-28 16:10:30 -0700

    Time: 60 ms (5.02%) | Memory: 17.7 MB (53.27%) - LeetSync

---
+++ 110-balanced-binary-tree/balanced-binary-tree.py
@@ -0,0 +1,39 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        # get depth of left node
+        # get depth of right node
+        # if difference >1, return False else return True
+        def getDepth(node):
+
+            if not node:
+                return [ True, 0]
+            
+            l=getDepth(node.left)
+            print('left', l, node.val)
+            if not l[0]:
+                return [False, l[1]+1]
+
+            r=getDepth(node.right)
+            print('right', r, node.val)
+            if not r[0]:
+                return [False, r[1]+1]
+
+            diff=abs(r[1]-l[1])
+            print('diff', diff, node.val)
+            height=max(l[1], r[1])
+            if diff>1:
+                return [False, height+1]
+            return [True, height+1]
+
+        res=getDepth(root)
+        print('res', res)
+        if not res[0]:
+            return False
+        else:
+            return True
