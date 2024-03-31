30676f560feb5ae612768b14d5f9c74c039b48a8
Yash Jain <36790403+Yash2108@users.noreply.github.com>
2024-03-30 17:05:05 -0700

    Time: 148 ms (5.04%) | Memory: 20.5 MB (39.44%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -2,13 +2,16 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         anagram_dict={}
         for string in strs:
-            chara_count={}
+            chara_count=[0 for i in range(26)]
             for chara in string:
-                if chara in chara_count:
-                    chara_count[chara]+=1
-                else:
-                    chara_count[chara]=1
-            chara_count_str=''.join([str(key)+str(val) for key, val in sorted(chara_count.items(), key=lambda k: k[0])])
+                chara_count[ord(chara)-97]+=1
+                # if chara in chara_count:
+                #     chara_count[chara]+=1
+                # else:
+                #     chara_count[chara]=1
+
+            chara_count_str=''.join([ chr(idx+97)+str(count) for idx, count in enumerate(chara_count)])
+            # chara_count_str=''.join([str(key)+str(val) for key, val in sorted(chara_count.items(), key=lambda k: k[0])])
             
             if chara_count_str in anagram_dict.keys():
                 anagram_dict[chara_count_str].append(string)
