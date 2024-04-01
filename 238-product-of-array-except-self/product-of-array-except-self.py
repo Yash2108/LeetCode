255c310bc387a7ed1d4c2d7619a3c2435b9c4ebd
Yash Jain <36790403+Yash2108@users.noreply.github.com>
2024-03-31 20:44:40 -0700

    Time: 255 ms (9.29%) | Memory: 25.8 MB (7.06%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -5,11 +5,8 @@ class Solution:
             output[i+1]=nums[i]*output[i]
 
         post=1
-        # for i in range(len(nums)-1, -1, -1):
-        #     output[i]*=post
-        #     post*=nums[i]
-        for i in range(1, len(nums)+1):
-            output[len(nums)-i]*=post
-            post*=nums[len(nums)-i]
+        for i in range(len(nums)-1, -1, -1):
+            output[i]*=post
+            post*=nums[i]
 
         return output
