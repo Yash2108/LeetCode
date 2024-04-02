ccb04f96e70099d6574d5cd39cb83042007686d4
Yash Jain <36790403+Yash2108@users.noreply.github.com>
2024-04-02 00:18:37 -0700

    Time: 4388 ms (20.25%) | Memory: 31.8 MB (50.03%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -6,11 +6,9 @@ class Solution:
             if num-1 not in num_set:
                 # make a seq starting with this
                 length=1
-                next_num=num+1
-                while next_num in num_set:
+                while num+length in num_set:
                     # add next_num to seq
                     length+=1
-                    next_num+=1
                 max_len=max(max_len, length)
 
         return max_len
