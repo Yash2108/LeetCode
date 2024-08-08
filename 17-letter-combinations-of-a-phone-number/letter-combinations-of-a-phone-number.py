2bd780c06b277b7bb7ebad991d876f260bab5820
Yash Jain <36790403+Yash2108@users.noreply.github.com>
2024-08-08 13:39:43 -0400

    Time: 30 ms (86.88%) | Memory: 16.6 MB (8.37%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,26 +1,25 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        if not digits:
-            return ''
-
-        digit_mapping={
-            '2':['a', 'b', 'c'],
-            '3':['d', 'e', 'f'],
-            '4':['g', 'h', 'i'],
-            '5':['j', 'k', 'l'],
-            '6':['m', 'n', 'o'],
-            '7':['p', 'q', 'r', 's'],
-            '8':['t', 'u', 'v'],
-            '9':['w', 'x', 'y', 'z'],
+        output = []
+        digitToChar = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "qprs",
+            "8": "tuv",
+            "9": "wxyz",
         }
-        
-        combinations=[]
-        for character in digit_mapping[digits[0]]:
-            combinations.append(character)
-        for num in digits[1:]:
-            new_combinations=[]
-            for word in combinations:
-                for character in digit_mapping[num]:
-                    new_combinations.append(word+character)
-            combinations=new_combinations
-        return combinations
+
+        def backtrack(i, curStr):
+            if len(curStr) == len(digits):
+                output.append(curStr)
+                return
+            for c in digitToChar[digits[i]]:
+                backtrack(i + 1, curStr + c)
+
+        if digits:
+            backtrack(0, "")
+
+        return output
