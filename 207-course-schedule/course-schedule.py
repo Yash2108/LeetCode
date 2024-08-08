3bed9172c21932bc5b92af30338cd786db0cdf36
Yash Jain <36790403+Yash2108@users.noreply.github.com>
2024-08-08 02:13:41 -0400

    Time: 128 ms (7.72%) | Memory: 18.3 MB (46.44%) - LeetSync

---
+++ 207-course-schedule/course-schedule.py
@@ -0,0 +1,32 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        
+        courses={i:[] for i in range(numCourses)}
+        for course in prerequisites:
+            if course[0]==course[1]:
+                return False
+            courses[course[0]].append(course[1])
+        
+        visited=[]
+        seen=[False for _ in range(numCourses)]
+        def dfs(current):
+            if current in visited:
+                return True
+            if seen[current]:
+                return False
+
+            seen[current]=True
+
+            for neighbor in courses[current]:
+                if not dfs(neighbor):
+                    return False
+            visited.append(current)
+            seen[current]=False
+            return True
+        
+        for course in courses:
+            if course not in visited:
+                if not dfs(course):
+                    return False
+
+        return True
