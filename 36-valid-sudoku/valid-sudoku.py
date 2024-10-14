fe3c1d611025c74d5efd58603cd61463ad1291a3
Yash Jain <36790403+Yash2108@users.noreply.github.com>
2024-10-14 04:19:46 -0400

    Time: 96 ms (32.88%) | Memory: 16.5 MB (59.97%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,26 +1,24 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        '''
-        r1, r2, ..., r9
-        c1, c2, ..., c9
-        b1, b2, ..., b9
-        '''
-
         rules={}
         for i in range(9):
             rules['r'+str(i)]=[]
             rules['c'+str(i)]=[]
-            rules['b'+str(i//3)+str(i%3)]=[]
-        
-        for row_idx in range(len(board)):
-            for col_idx in range(len(board[row_idx])):
-                val=board[row_idx][col_idx]
-                if val=='.':
+        for i in range(3):
+            for j in range(3):
+                rules['b'+str(i)+str(j)]=[]
+
+        for i in range(9):
+            for j in range(9):
+                if board[i][j]==".":
                     continue
-                if val in rules['r'+str(row_idx)] or val in rules['c'+str(col_idx)] or val in rules['b'+str(row_idx//3)+str(col_idx//3)]:
+                if board[i][j] in rules['r'+str(i)] or\
+                    board[i][j] in rules['c'+str(j)] or \
+                    board[i][j] in rules['b'+str(i//3)+str(j//3)]:
                     return False
                 else:
-                    rules['r'+str(row_idx)].append(val)
-                    rules['c'+str(col_idx)].append(val)
-                    rules['b'+str(row_idx//3)+str(col_idx//3)].append(val)
+                    rules['r'+str(i)].append(board[i][j])
+                    rules['c'+str(j)].append(board[i][j])
+                    rules['b'+str(i//3)+str(j//3)].append(board[i][j])
+                    
         return True
