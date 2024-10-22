8c967cb20f76b1e695c2d0e0a91740601a8e4667
Yash Jain <36790403+Yash2108@users.noreply.github.com>
2024-10-21 23:07:02 -0400

    Time: 326 ms (5.13%) | Memory: 31.2 MB (48.12%) - LeetSync

---
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        visited=[]
+        toVisit=[[root]]
+        while toVisit:
+            node=toVisit.pop(0)
+            if not node:
+                continue
+            if max([i.val for i in node])<=node[-1].val:
+                visited.append(node)
+            if node[-1].left:
+                new_nodes=node+[node[-1].left]
+                toVisit.append(new_nodes)
+            if node[-1].right:
+                new_nodes=node+[node[-1].right]
+                toVisit.append(new_nodes)
+        return len(visited)
+'''
+
+maintain 2 lists: tovisit and visited
+visited will have the result stored as lists
+toVisit will store the next item to check as well as its history
+loop while toVisit has something
+update
+
+'''
