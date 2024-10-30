5ecd46d3294911071f860fe2bc324e627b2ca483
Yash Jain <36790403+Yash2108@users.noreply.github.com>
2024-10-29 21:01:16 -0400

    Time: 55 ms (54.65%) | Memory: 20.4 MB (12.16%) - LeetSync

---
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
@@ -0,0 +1,46 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        
+
+        '''
+        
+        lets do dfs
+        we search for p and store its ancestors in a stack
+        then from this list of ancestors, we search for q.
+        return the ancestor when we have found q 
+
+        '''
+
+        ancestors=[]
+
+        current_node=root
+        while current_node:
+            ancestors.append(current_node)
+            if current_node.val==p.val:
+                break
+            elif current_node.val<p.val:
+                current_node=current_node.right
+            else:
+                current_node=current_node.left
+        ancestors=ancestors[::-1]
+
+        while ancestors:
+            node=ancestors[0]
+
+            while node:
+                if node.val==q.val:
+                    return ancestors[0]
+                elif node.val<q.val:
+                    node=node.right
+                else:
+                    node=node.left 
+            ancestors.pop(0)
+        
+        # print([i.val for i in ancestors])
