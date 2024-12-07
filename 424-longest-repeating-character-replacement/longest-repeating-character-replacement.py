119eead510bd717d917adf0f11ea9ddc17d81c87
Yash Jain <36790403+Yash2108@users.noreply.github.com>
2024-12-07 17:58:57 -0500

    Time: 183 ms (14.19%) | Memory: 17.7 MB (5.33%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,27 +1,27 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        replacements = 0
         current_idx = 0
         next_idx = 0
         longest=0
         current_len=0
         charCount={}
-        while next_idx<len(s):
+
+
+        for next_idx in range(len(s)):
             if s[next_idx] in charCount:
                 charCount[s[next_idx]]+=1
             else:
                 charCount[s[next_idx]]=1
             
-            freq_char = sorted(charCount.items(), key = lambda x:x[1])[-1]
-            replacements_reqd = next_idx-current_idx+1 - freq_char[1]
+            freq_char = max(charCount.values())
+            replacements_reqd = next_idx-current_idx+1 - freq_char
 
             while replacements_reqd>k:
                 charCount[s[current_idx]]-=1
-                freq_char = sorted(charCount.items(), key = lambda x:x[1])[-1]
-                replacements_reqd = next_idx-current_idx - freq_char[1]
                 current_idx+=1
+                freq_char = max(charCount.values())
+                replacements_reqd = next_idx-current_idx+1 - freq_char
                     
             current_len=next_idx-current_idx+1
             longest=max(longest, current_len)
-            next_idx+=1
         return longest
