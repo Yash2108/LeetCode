3174e67d973633dfd8b705b30132bf2ef46d9e48
Yash Jain <36790403+Yash2108@users.noreply.github.com>
2024-12-07 17:31:12 -0500

    Time: 474 ms (5.01%) | Memory: 17.7 MB (5.33%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,13 +1,27 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        start_point=0
-        count=defaultdict(lambda: 0)
-        longest_len = 0
-        for i in range(len(s)):
-            count[s[i]]+=1
-            while len(s[start_point:i+1])-max(count.items(), key = lambda x:x[1])[1] > k:
-                count[s[start_point]]-=1
-                start_point+=1
-            longest_len = max(longest_len, i-start_point+1)
+        replacements = 0
+        current_idx = 0
+        next_idx = 0
+        longest=0
+        current_len=0
+        charCount={}
+        while next_idx<len(s):
+            if s[next_idx] in charCount:
+                charCount[s[next_idx]]+=1
+            else:
+                charCount[s[next_idx]]=1
+            
+            freq_char = sorted(charCount.items(), key = lambda x:x[1])[-1]
+            replacements_reqd = next_idx-current_idx+1 - freq_char[1]
 
-        return longest_len
+            while replacements_reqd>k:
+                charCount[s[current_idx]]-=1
+                freq_char = sorted(charCount.items(), key = lambda x:x[1])[-1]
+                replacements_reqd = next_idx-current_idx - freq_char[1]
+                current_idx+=1
+                    
+            current_len=next_idx-current_idx+1
+            longest=max(longest, current_len)
+            next_idx+=1
+        return longest
