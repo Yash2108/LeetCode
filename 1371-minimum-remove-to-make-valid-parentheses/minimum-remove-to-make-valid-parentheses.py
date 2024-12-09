6b36d87a458cfaf506d1be1b7dc75d2d8f587dff
Yash Jain <36790403+Yash2108@users.noreply.github.com>
2024-12-08 23:12:56 -0500

    Time: 669 ms (5.04%) | Memory: 19.5 MB (13.68%) - LeetSync

---
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
@@ -0,0 +1,35 @@
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        stack = []
+        mapping={
+            ')':'(',
+            ']':'[',
+            '}':'{'
+        }
+        alphabets='abcdefghijklmnopqrstuvwxyz'
+        output=''
+        removals=[]
+        for idx, chara in enumerate(s):
+            output+=chara
+            if chara in alphabets:
+                continue
+            if chara not in mapping:
+                stack.append([chara, idx])
+            else:
+                if stack:
+                    if stack[-1][0]!=mapping[chara]:
+                        removal.append(stack[-1][1])
+                        removal.append(idx)
+                    stack.pop(-1)
+                else:
+                    removals.append(idx)
+        print(output)
+
+        while stack:
+            bracket= stack.pop(-1)
+            removals.append(bracket[1])
+        final_output=[]
+        for idx in range(len(s)):
+            if idx not in removals:
+                final_output.append(s[idx])
+        return ''.join(final_output)
