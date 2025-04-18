c08eab4cb8c620eedd85b63090ba060bd7a351f8
Yash Jain <36790403+Yash2108@users.noreply.github.com>
2025-04-18 14:46:53 -0400

    Time: 11 ms (83.06%) | Memory: 20.2 MB (99.43%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -3,12 +3,9 @@ class Solution:
         anagram_map = {}
 
         for word in strs:
-            word_map = [0 for _ in range(26)]
-            for chara in word:
-                word_map[ord(chara)-ord('a')]+=1
-            word_map=''.join([chr(w+ord('a'))+str(c) for w, c in enumerate(word_map)])
-            if word_map in anagram_map:
-                anagram_map[word_map].append(word)
+            sorted_word = ''.join(sorted(word))
+            if sorted_word in anagram_map:
+                anagram_map[sorted_word].append(word)
             else:
-                anagram_map[word_map]=[word]
-        return [ anagrams for anagrams in anagram_map.values()]
+                anagram_map[sorted_word]=[word]
+        return list(anagram_map.values())
