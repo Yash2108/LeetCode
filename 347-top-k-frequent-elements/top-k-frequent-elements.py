09576f5c29daf2c6ccd7bb28f4a348f540d46390
Yash Jain <36790403+Yash2108@users.noreply.github.com>
2025-04-18 18:14:39 -0400

    Time: 12 ms (15.41%) | Memory: 24.2 MB (19.85%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,16 +1,18 @@
-from heapq import heapify, heappush, heappop
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        buckets=[[] for _ in range(len(nums))]
         num_freq={}
         for num in nums:
             if num in num_freq:
                 num_freq[num]+=1
             else:
                 num_freq[num]=1
-        heapify_ls=[]
-        heapify(heapify_ls)
         for num in num_freq:
-            heappush(heapify_ls, [num_freq[num], num])
-            if len(heapify_ls)>k:
-                heappop(heapify_ls)
-        return [i[1] for i in heapify_ls[::-1]]
+            buckets[num_freq[num]-1].append(num)
+        result=[]
+        for bucket in buckets[::-1]:
+            for num in bucket:
+                if len(result)==k:
+                    return result
+                result.append(num)
+        return result
