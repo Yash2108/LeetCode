4b961b2d28f221a4784a412e06acdcd7f32eb33f
Yash Jain <36790403+Yash2108@users.noreply.github.com>
2025-04-19 15:16:23 -0400

    Time: 48 ms (5.82%) | Memory: 17.7 MB (74.01%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -1,15 +1,20 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
         result=''
-        toggle = True
-        while word1 and word2:
-            if toggle:
-                result+=word1[0]
-                word1=word1[1:]
-            else:
-                result+=word2[0]
-                word2=word2[1:]
-            toggle=not toggle
+        # toggle = True
+        for i in range(min(len(word1), len(word2))):
+            result+=word1[0]+word2[0]
+            word1=word1[1:]
+            word2=word2[1:]
+        
+        # while word1 and word2:
+        #     if toggle:
+        #         result+=word1[0]
+        #         word1=word1[1:]
+        #     else:
+        #         result+=word2[0]
+        #         word2=word2[1:]
+        #     toggle=not toggle
         if word1:
             result+=word1
         else:
