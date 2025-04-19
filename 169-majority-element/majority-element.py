4a4984510294d3677c5ad81129981ab9414a6c2a
Yash Jain <36790403+Yash2108@users.noreply.github.com>
2025-04-19 14:22:24 -0400

    Time: 0 ms (100.00%) | Memory: 19.2 MB (87.46%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,15 +1,9 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        num_freq={}
+        result, count = 0, 0
+        
         for num in nums:
-            if num in num_freq:
-                num_freq[num]+=1
-            else:
-                num_freq[num]=1
-        max_freq=0
-        max_num=None
-        for i in num_freq:
-            if num_freq[i]>max_freq:
-                max_num=i
-                max_freq=num_freq[i]
-        return max_num
+            if count==0:
+                result=num
+            count+= 1 if num==result else -1
+        return result
