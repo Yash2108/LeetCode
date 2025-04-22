f0352e04fb799cdd6173ea3cfa8c2acadc9a8661
Yash Jain <36790403+Yash2108@users.noreply.github.com>
2025-04-22 11:56:44 -0400

    Time: 20 ms (40.42%) | Memory: 17.9 MB (34.44%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,22 +1,14 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        '''
-        longest variable
-        two pointers
-        one pointer indicates start of substring
-        other pointer keeps going forward till a repeat occurs
-        for checking repeats, we can use a set so the check is O(1)
-        '''
-
+        substring=dict()
         longest=0
-        start=end=0
-        hashmap={}
-        while end!=len(s): 
-            while s[end] in hashmap:  
-                del hashmap[s[start]] 
-                start+=1 
-                longest=max(longest, len(hashmap)) 
-            hashmap[s[end]]=end 
-            longest=max(longest, len(hashmap))
-            end+=1 
+        start, end = 0, 0
+
+        while end!=len(s):
+            while s[end] in substring:
+                del substring[s[start]]
+                start+=1
+            substring[s[end]]=end
+            longest = max(longest, len(substring))
+            end+=1
         return longest
