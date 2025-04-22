b34a7ecbd194d7ccffedcb8df0dbac961747d366
Yash Jain <36790403+Yash2108@users.noreply.github.com>
2025-04-22 14:07:13 -0400

    Time: 1 ms (63.36%) | Memory: 17.8 MB (75.46%) - LeetSync

--- 778-reorganize-string/reorganize-string.py
+++ 778-reorganize-string/reorganize-string.py
@@ -1,24 +1,37 @@
 from heapq import heapify, heappush, heappop
 class Solution:
     def reorganizeString(self, s: str) -> str:
+        '''
+
+        we can take count of all characters.
+        have a result string. 
+        we always add the most frequent character to the string.
+        we cant repeat characters so we keep note of the last used.
+        keep adding till we either run out of characters or our only choice is to repeat.
+
+        '''
 
         freq=Counter(s)
-        freq=[[-cnt, chara]for chara, cnt in freq.items()]
+        freq=list(freq.items())
+        freq = [[-i[1], i[0]] for i in freq]
         heapify(freq)
 
-        popped=None
         result=''
-        while freq or popped:
-            if popped and not freq:
-                return ''
-            cnt, chara=heappop(freq)
-            result+=chara
-            cnt+=1
+        print(freq)
+        last_used=None
 
-            if popped:
-                heappush(freq, popped)
-                popped=None
-            if cnt!=0:
-                popped=[cnt, chara]
+        while freq: 
+            most_freq=heappop(freq)
+            if result and result[-1]==most_freq[1]: 
+                return ''
+
+            result+=most_freq[1] 
+            most_freq[0]+=1 
+            if last_used and last_used[0]<0: 
+                heappush(freq, last_used) 
+            last_used = most_freq 
         
+        if last_used[0]<0:
+            return ''
+            
         return result
