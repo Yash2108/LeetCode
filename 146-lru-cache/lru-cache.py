b162bb0190c68740b4ba9a0a685113cf99665c23
Yash Jain <36790403+Yash2108@users.noreply.github.com>
2025-04-22 22:35:26 -0400

    Time: 166 ms (28.43%) | Memory: 78.4 MB (63.41%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,36 +1,84 @@
+class Node:
+
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.prev = self.next = None
+    
+    
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.capacity = capacity
         self.cache={}
-        self.oldest=[]
+        self.least_recent, self.most_recent = Node(0, 0), Node(0, 0)
+        self.least_recent.next, self.most_recent.prev = self.most_recent, self.least_recent
+    
+    def insert(self, node):
+        prev, nxt = self.most_recent.prev, self.most_recent
+        
+        prev.next = nxt.prev = node
+        node.prev = prev
+        node.next = nxt
+
+        # new.p=most.prev
+        # new.n=most
+        # most.p.n=new
+        # most.p=new
+    
+    def remove(self, node):
+        node.prev.next = node.next
+        node.next.prev = node.prev
+
+
+    # def insert(self, node):
+    #     prev, nxt = self.most_recent.prev, self.most_recent
+    #     prev.next = nxt.prev = node
+    #     node.next, node.prev = nxt, prev
+
+        
+    #     self.most_recent.prev.next = self.most_recent.prev = node
+    #     node.prev = self.most_recent.prev
+    #     node.next=self.most_recent
+
 
     def get(self, key: int) -> int:
-        if key in self.cache:
-            idx=self.oldest.index(key)
-            update_key=self.oldest.pop(idx)
-            self.oldest.append(update_key)
+        '''
+        remove
+        insert
+        return
+        '''
 
-            return self.cache[key]
+        if key in self.cache:
+            #TODO: remove
+            self.remove(self.cache[key])
+            #TODO: insert
+            self.insert(self.cache[key])
+            return self.cache[key].value
         else:
             return -1
 
     def put(self, key: int, value: int) -> None:
-        if key not in self.cache:
+        '''
+        remove if exists
+        make node
+        insert at front
+        if cap hit, remove lru
+        '''
 
-            if len(self.cache)==self.capacity:
-                delete_key = self.oldest.pop(0)
-                del self.cache[delete_key]
-            
-            self.cache[key] = value
-            self.oldest.append(key)
-        
-        else:
-            self.cache[key] = value
+        if key in self.cache:
+            #TODO: remove
+            self.remove(self.cache[key])
+        #TODO: insert
+        # node=
+        self.cache[key]=Node(key, value)
+        self.insert(self.cache[key])
+        if len(self.cache)>self.capacity:
+            lru=self.least_recent.next
+            self.remove(lru)
+            del self.cache[lru.key]
+        #TODO: remove least
 
-            idx=self.oldest.index(key)
-            update_key=self.oldest.pop(idx)
-            self.oldest.append(update_key)
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
