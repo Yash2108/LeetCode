493b2bc801674482698ca6f22893986b2d83de7c
Yash Jain <36790403+Yash2108@users.noreply.github.com>
2025-04-22 23:44:50 -0400

    Time: 1569 ms (5.02%) | Memory: 19.7 MB (14.12%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -5,33 +5,21 @@ class Solution:
             mapping[course].append(prereq)
         
         def dfs(course, visited, seen):
+            if course in visited:
+                return True
+            if course in seen:
+                return False
             seen[course]=None
             for prereq in mapping[course]:
-                # seen[prereq]=None
-                if prereq in visited:
-                    continue
-                if prereq in seen:
-                    return False
                 if dfs(prereq, visited, seen):
                     visited.append(prereq)
-                    # del seen[prereq]
                 else:
                     return False
             del seen[course]
             visited.append(course)
             return True
-                    
 
         for course in range(numCourses):
             if not dfs(course, [], {}):
                 return False
         return True
-        '''
-
-        take course and prereq
-        add course to seen
-        if all prereq in visited, add course to visited and remove from seen
-        if prereq in seen, return False
-        after all courses have been iterated return True
-
-        '''
