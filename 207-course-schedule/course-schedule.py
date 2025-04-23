eb959820f9df7e141a339b92215632377f125259
Yash Jain <36790403+Yash2108@users.noreply.github.com>
2025-04-22 23:41:31 -0400

    Time: 1551 ms (5.02%) | Memory: 19.5 MB (31.28%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,32 +1,37 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        mapping={i:[] for i in range(numCourses)}
+        for course, prereq in prerequisites:
+            mapping[course].append(prereq)
         
-        courses={i:[] for i in range(numCourses)}
-        for course in prerequisites:
-            # if course[0]==course[1]:
-            #     return False
-            courses[course[0]].append(course[1])
-        
-        visited=[]
-        seen=[False for _ in range(numCourses)]
-        def dfs(current):
-            if current in visited:
-                return True
-            if seen[current]:
-                return False
-
-            seen[current]=True
-
-            for neighbor in courses[current]:
-                if not dfs(neighbor):
+        def dfs(course, visited, seen):
+            seen[course]=None
+            for prereq in mapping[course]:
+                # seen[prereq]=None
+                if prereq in visited:
+                    continue
+                if prereq in seen:
                     return False
-            visited.append(current)
-            seen[current]=False
+                if dfs(prereq, visited, seen):
+                    visited.append(prereq)
+                    # del seen[prereq]
+                else:
+                    return False
+            del seen[course]
+            visited.append(course)
             return True
-        
-        for course in courses:
-            if course not in visited:
-                if not dfs(course):
-                    return False
+                    
 
+        for course in range(numCourses):
+            if not dfs(course, [], {}):
+                return False
         return True
+        '''
+
+        take course and prereq
+        add course to seen
+        if all prereq in visited, add course to visited and remove from seen
+        if prereq in seen, return False
+        after all courses have been iterated return True
+
+        '''
