46818254685624fd07307ccfea579961587ef9da
Yash Jain <36790403+Yash2108@users.noreply.github.com>
2025-04-24 15:05:01 -0400

    Time: 4 ms (96.83%) | Memory: 20.4 MB (36.11%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -1,26 +1,23 @@
+from heapq import heapify, heappush, heappop
+
 # Definition for singly-linked list.
-import heapq
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        while None in lists:
-            lists.pop(lists.index(None))
-        if len(lists)==0:
-            return 
+        heap=[]
+        heapify(heap)
 
-        heap = []
-        heapq.heapify(heap)
         for ls in lists:
             while ls:
-                heapq.heappush(heap, ls.val)
+                heappush(heap, ls.val)
                 ls=ls.next
-
-        initial_node=ListNode()
-        new_node=initial_node
+        result=ListNode(None, None)
+        head=result
         while heap:
-            new_node.next=ListNode(val=heapq.heappop(heap))
-            new_node=new_node.next
-        return initial_node.next
+            new_node = ListNode(heappop(heap))
+            head.next = new_node
+            head = head.next
+        return result.next
